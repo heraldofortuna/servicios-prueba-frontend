59836c481efecd9a9de3b376226770c421f16da5
Heraldo Fortuna <heraldofortuna@gmail.com>
2022-09-06 18:32:39 -0500

    Guardo por cambio de pc local

--- src/components/ServiceForm.jsx
+++ src/components/ServiceForm.jsx
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Button from "../components/Button";
 
 const ServiceForm = () => {
+  const [service, setService] = useState({
+    title: "",
+    description: "",
+    type: "home",
+  });
+  const [type, setType] = useState("");
+
+  const handleChange = (event) => {
+    console.log("handleChange:", event);
+  };
+
+  const handleSelectValue = (event) => {
+    console.log("handleSelectValue:", event.target);
+  };
+
   return (
     <form>
       <h2>Servicio</h2>
       <label>
         Nombre
-        <input />
+        <input name="name" type="text" onChange={handleChange} />
+      </label>
+      <label>
+        Tipo
+        <select value={type} onChange={handleSelectValue}>
+          <option value={"cars"}>Autos</option>
+          <option value={"health"}>Salud</option>
+          <option value={"home"}>Hogar</option>
+        </select>
       </label>
       <label>
         Descripción
-        <input />
+        <textarea name="description" onChange={handleChange} />
       </label>
       <div>
         <Button text={"Grabar"} />
